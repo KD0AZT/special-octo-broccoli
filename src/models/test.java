3a16bb4f3360eba37934b4f3643e21551b4cc2c8
KD0AZT <kd0azt@gmail.com>
2021-03-19 16:24:15 -0700

    Added a while loop to the test file

--- src/models/test.java
+++ src/models/test.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 public class test {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        int answer;        
-        System.out.println("What is the answer?");
-        answer = keyboard.nextInt();
-        if (answer == 42) {
-            System.out.println("Don't forget your towel!");
-        }
-        else {
-            System.out.println("Nice Try!");
+        int answer = 0;        
+        while (answer != 42) {
+            System.out.println("What is the answer?");
+            answer = keyboard.nextInt();
+            if (answer == 42) {
+                System.out.println("Don't forget your towel!");
+            }
+            else {
+                System.out.println("Nice Try!");
+            }
         }
         keyboard.close();
     }
