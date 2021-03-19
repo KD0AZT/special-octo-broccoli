cfc4ab068168130249737ad1adaa1efc7df8209f
KD0AZT <33142442+KD0AZT@users.noreply.github.com>
2021-03-19 16:33:37 -0700

    added whitespace

--- src/models/test.java
+++ src/models/test.java
@@ -9,9 +9,11 @@ public class test {
             answer = keyboard.nextInt();
             if (answer == 42) {
                 System.out.println("Don't forget your towel!");
+                System.out.println();
             }
             else {
                 System.out.println("Nice Try!");
+                System.out.println();
             }
         }
         keyboard.close();
