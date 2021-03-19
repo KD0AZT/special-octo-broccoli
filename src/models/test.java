38e7fdb709e72977d9318517f3c1c580db75e44b
KD0AZT <kd0azt@gmail.com>
2021-03-19 16:21:34 -0700

    test commit 2

--- src/models/test.java
+++ src/models/test.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class test {
     public static void main(String[] args) {
-        System.out.println("42");
+        Scanner keyboard = new Scanner(System.in);
+        int answer;        
+        System.out.println("What is the answer?");
+        answer = keyboard.nextInt();
+        if (answer == 42) {
+            System.out.println("Don't forget your towel!");
+        }
+        else {
+            System.out.println("Nice Try!");
+        }
+        keyboard.close();
     }
 }
